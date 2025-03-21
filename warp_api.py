8100701da1de27cc8d477d2df681c1d8873d6224
Anubis <anujagmage@gmail.com>
2025-03-21 15:17:07 -0300

    uncomment code

--- warp_api.py
+++ warp_api.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI, File, UploadFile
 from fastapi.responses import JSONResponse
-#from report_generation_final import process_transcript
+from report_generation_final import process_transcript
 import json
 
 from starlette.middleware.cors import CORSMiddleware
@@ -15,29 +15,29 @@ app.add_middleware(
 )
 def llm_process(text: str) -> dict:
 
-    # bert_model_name = "d4data/biomedical-ner-all"
-    # ollama_model = "phi4:latest"
-    # output_json = process_transcript(text, bert_model_name, ollama_model)
-    # return output_json
-    test = {
-          "patient_info": {
-            'NAME': 'ANONIMIZED',
-            'SEX': 'FEMALE',
-            'AGE': '28',
-            'DATE': '15 JAN 2025',
-            'UHID.NO': '',
-            'REF. By': 'OBSTETRICS DEPARTMENT',
-            'OTHER': {"EXAMINATION":"GRAV IDU TERUS","LMP":"15/11/2024","D":"7 WEEKS 3 DAYS", "EDD":"22/08/2025"}
-          },
-          "findings": ["The grav id uterus shows a smooth walled gestation al sac",
-            "A fetal pole is seen with cardiac activity appreciated",
-          "Decidual reaction is noted to be good",
-          "CRL measures 3 mm, suggesting an estimated gestational age of approximately 5 weeks 6 days",
-          "A tiny blood collection is observed just above the internal os, measuring 5.9 x 2.8 mm"
-          ],
-          "comments": ["Early intrauterine pregnancy estimated at about 5 weeks and 6 days. The presence of delayed conception is noted, with further examination planned for UIGDD on 30 August 2025"]
-        }
-    return test
+    bert_model_name = "d4data/biomedical-ner-all"
+    ollama_model = "phi4:latest"
+    output_json = process_transcript(text, bert_model_name, ollama_model)
+    return output_json
+    # test = {
+    #       "patient_info": {
+    #         'NAME': 'ANONIMIZED',
+    #         'SEX': 'FEMALE',
+    #         'AGE': '28',
+    #         'DATE': '15 JAN 2025',
+    #         'UHID.NO': '',
+    #         'REF. By': 'OBSTETRICS DEPARTMENT',
+    #         'OTHER': {"EXAMINATION":"GRAV IDU TERUS","LMP":"15/11/2024","D":"7 WEEKS 3 DAYS", "EDD":"22/08/2025"}
+    #       },
+    #       "findings": ["The grav id uterus shows a smooth walled gestation al sac",
+    #         "A fetal pole is seen with cardiac activity appreciated",
+    #       "Decidual reaction is noted to be good",
+    #       "CRL measures 3 mm, suggesting an estimated gestational age of approximately 5 weeks 6 days",
+    #       "A tiny blood collection is observed just above the internal os, measuring 5.9 x 2.8 mm"
+    #       ],
+    #       "comments": ["Early intrauterine pregnancy estimated at about 5 weeks and 6 days. The presence of delayed conception is noted, with further examination planned for UIGDD on 30 August 2025"]
+    #     }
+    # return test
 
 @app.post("/process-file/")
 async def process_file(file: UploadFile = File(...)):
